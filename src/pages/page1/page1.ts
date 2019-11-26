dc540b148a70107b6fa4adf5d32dfc2dc207006c
ravierdeoliveira <ravierdeoliveira@infojr.com.br>
2019-11-26 12:29:31 -0300

    Exercicio Finalizado

--- src/pages/page1/page1.ts
+++ src/pages/page1/page1.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -7,9 +7,29 @@ import { IonicPage, NavController } from 'ionic-angular';
   templateUrl: 'page1.html'
 })
 export class Page1 {
+  inicio: string = "Oi, "
+  fim: string = "!"
+  pessoa: any = "Fulano";
+  
+  constructor(public navCtrl: NavController, public navParams: NavParams){  }
 
-  constructor(public navCtrl: NavController) {
 
+  goToPage2(){   
+    this.navCtrl.push('Page2',{
+        'callback': this.receberDados.bind(this),
+        'atual': this.pessoa
+    });
   }
+
+  receberDados(d:string){
+    if(d != undefined && d != '')
+      this.pessoa = d;
+    else if (d == ''){
+      this.inicio = "Oi!";
+      this.pessoa = "";
+      this.fim = "";
+    }
+  }
+
 
 }
--- src/pages/page2/page2.ts
+++ src/pages/page2/page2.ts
@@ -7,32 +7,22 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'page2.html'
 })
 export class Page2 {
-  selectedItem: any;
-  icons: string[];
-  items: Array<{ title: string, note: string, icon: string }>;
-
+  callback: any
+  usuarioAtual: any;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    // If we navigated to this page, we will have an item available as a nav param
-    this.selectedItem = navParams.get('item');
+    this.usuarioAtual = this.navParams.get('atual');
+    this.callback = this.navParams.get("callback");
+  }  
 
-    // Let's populate this page with some filler content for funzies
-    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-      'american-football', 'boat', 'bluetooth', 'build'];
 
-    this.items = [];
-    for (let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-      });
-    }
+
+  goToPage1(t:string){
+    this.callback(this.usuarioAtual);
+    this.navCtrl.pop()
   }
 
-  itemTapped(event, item) {
-    // That's right, we're pushing to ourselves!
-    this.navCtrl.push('Page2', {
-      item: item
-    });
+  cancel(){
+    this.navCtrl.pop();
   }
 }
+
